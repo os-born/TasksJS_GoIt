e7c59446a56646085596621a1a1ee2d5d870b905
Oleg Serdiuk <ol.serduk@gmail.com>
2021-05-22 16:16:13 +0300

    18 задача

---
+++ tasks.js
@@ -0,0 +1,225 @@
+// ============Module1===========
+
+// ============TASK1===========
+// Change code below this line
+// const productName = 'Droid';
+// console.log(productName);
+
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+
+// ============TASK2===========
+// let productName = "Droid";
+// let pricePerItem = 2000;
+
+// // Change code below this line
+// productName = "Repair droid";
+// pricePerItem = (pricePerItem + 1500);
+
+// ============TASK3===========
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// ============TASK4===========
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+
+// // Change code below this line
+// const totalPrice = (pricePerItem * orderedQuantity);
+
+// ============TASK5===========
+// const productName = "Droid";
+// const pricePerItem = 3500;
+
+// // Change code below this line
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+
+// // ============TASK6===========
+// // Change code below this line
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = ((pricePerDroid*orderedQuantity)+deliveryFee)
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`
+
+// ============TASK7===========
+// // Change code below this line
+// function sayHi () {
+// console.log("Hello, this is my first function!")
+// };
+
+// sayHi();
+
+// ============TASK8===========
+// // Change code below this line
+// function add(a,b,c) {
+//   console.log(`Addition result equals ${(a + b + c)}`);
+//   // Change code above this line
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// ============TASK9===========
+// function add(a, b, c) {
+//   // Change code below this line
+// return a + b + c;
+
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// ============TASK10===========
+// function makeMessage (name, price) {
+//   // Change code below this line
+//    const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// };
+
+// ============TASK11===========
+// function calculateTotalPrice (orderedQuantity, pricePerItem) {
+//   // Change code below this line
+//   const totalPrice = orderedQuantity*pricePerItem;
+
+//   // Change code above this line
+//   return totalPrice;
+// };
+
+// ============TASK12===========
+// function makeOrderMessage(orderedQuantity, pricePerDroid, deliveryFee) {
+//   // Change code below this line
+//   totalPrice = (orderedQuantity*pricePerDroid)+deliveryFee;
+// message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+//   // Change code above this line
+//   return message;
+// }
+
+// ============TASK13===========
+// function isAdult(age) {
+//   // Change code below this line
+//   const passed = (age>=18);
+
+//   // Change code above this line
+//   return passed;
+// }
+
+// ============TASK14===========
+// function isValidPassword(password) {
+//   const SAVED_PASSWORD = 'jqueryismyjam';
+//   // Change code below this line
+//   const isMatch = (password===SAVED_PASSWORD);
+
+//   // Change code above this line
+//   return isMatch;
+// }
+
+// ============TASK15===========
+// function checkAge(age) {
+//   let message;
+
+//   if (age>=18) { // Change this line
+//     message = 'You are an adult';
+//   } else {
+//     message = 'You are a minor';
+//   }
+
+//   return message;
+// }
+
+// ============TASK16===========
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
+// if (available<ordered){
+//   message = ("Not enough goods in stock!")
+// } else 
+//   message = ("Order is processed, our manager will contact you.")
+//   // Change code above this line
+//   return message;
+// }
+
+
+// ============TASK17===========
+// let a = 5;
+// let b = 10;
+// let c = 15;
+// let d = 20;
+
+// // Change code below this line
+// a += 2;
+// b -= 4;
+// c *= 3;
+// d /= 10;
+
+// ============TASK18===========
+
+
+// ============TASK19===========
+// ============TASK20===========
+// ============TASK21===========
+// ============TASK22===========
+// ============TASK23===========
+// ============TASK24===========
+// ============TASK25===========
+// ============TASK26===========
+// ============TASK27===========
+// ============TASK28===========
+// ============TASK29===========
+// ============TASK30===========
+// ============TASK31===========
+// ============TASK32===========
+// ============TASK33===========
+// ============TASK34===========
+// ============TASK35===========
+// ============TASK36===========
+
+
+
+// ============Module2===========
+
+// ============TASK1============
+// ============TASK2============
+// ============TASK3============
+// ============TASK4============
+// ============TASK5============
+// ============TASK6============
+// ============TASK7============
+// ============TASK8============
+// ============TASK9============
+// ============TASK10===========
+// ============TASK11===========
+// ============TASK12===========
+// ============TASK13===========
+// ============TASK14===========
+// ============TASK15===========
+// ============TASK16===========
+// ============TASK17===========
+// ============TASK18===========
+// ============TASK19===========
+// ============TASK20===========
+// ============TASK21===========
+// ============TASK22===========
+// ============TASK23===========
+// ============TASK24===========
+// ============TASK25===========
+// ============TASK26===========
+// ============TASK27===========
+// ============TASK28===========
+// ============TASK29===========
+// ============TASK30===========
+// ============TASK31===========
+// ============TASK32===========
+// ============TASK33===========
+// ============TASK34===========
+// ============TASK35===========
+// ============TASK36===========
