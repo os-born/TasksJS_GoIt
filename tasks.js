76cd7a21d5c5cb49a5197ad86134ebc409914e65
os-born <ol.serduk@gmail.com>
2021-05-30 20:49:06 +0300

    Task 16

--- tasks.js
+++ tasks.js
@@ -444,12 +444,55 @@
   
 
 // ============TASK3============
+// function checkStorage(available, ordered) {
+//     // Change code below this line
+//      if (ordered === 0) {
+//       return "Your order is empty!";
+//     }
+//     if (ordered > available) {
+//       return "Your order is too large, not enough goods in stock!";
+//        }
+//     return "The order is accepted, our manager will contact you";
+//     // Change code above this line
+//   }
+  
 // ============TASK4============
+// // Change code below this line
+// const fruits = ["apple", "plum", "pear", "orange"]
+
 // ============TASK5============
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// // Change code below this line
+
+
+// const firstElement = fruits[0];
+// const secondElement = fruits[1];
+// const lastElement = fruits[3];
+
 // ============TASK6============
+// const fruits = ["apple", "plum", "pear", "orange"];
+
+// // Write your code under this line
+// fruits[1] = "peach";
+// fruits[3] = "banana";
+
 // ============TASK7============
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const fruitsArrayLength = fruits.length;
+
 // ============TASK8============
+// const fruits = ["apple", "peach", "pear", "banana"];
+
+// // Change code below this line
+// const lastElementIndex = fruits.length - 1;
+// const lastElement = fruits[lastElementIndex];
+
 // ============TASK9============
+
+
 // ============TASK10===========
 // ============TASK11===========
 // ============TASK12===========
