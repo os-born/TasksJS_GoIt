8da9a9cf401f79a45cb5f9813d277197af8773d4
os-born <ol.serduk@gmail.com>
2021-06-20 15:54:07 +0300

    4 module done

--- tasks.js
+++ tasks.js
@@ -1447,11 +1447,90 @@
   
 
 // ============TASK4============
+// const pizzaPalace = {
+//     pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//     order(pizzaName, onSuccess, onError) {
+//     if (this.pizzas.includes(pizzaName)){
+//     return makePizza(pizzaName)
+//   }
+//     return onOrderError(pizzaName)
+  
+//       },
+//   };
+//   // Пиши код выше этой строки
+  
+//   // Колбэк для onSuccess
+//   function makePizza(pizzaName) {
+//     return `Ваш заказ принят. Готовим пиццу ${pizzaName}.`;
+//   }
+  
+//   // Колбэк для onError
+//   function onOrderError(error) {
+//     return `Ошибка! В ассортименте нет пиццы с названием ${error}.`;
+//   }
+  
+//   // Вызовы метода с колбэками
+//   pizzaPalace.order('Аль Копчино', makePizza, onOrderError);
+//   pizzaPalace.order('Четыре нарезона', makePizza, onOrderError);
+//   pizzaPalace.order('Биг майк', makePizza, onOrderError);
+//   pizzaPalace.order('Венская', makePizza, onOrderError);
 
 
 // ============TASK5============
+// const pizzaPalace = {
+//     pizzas: ['Ультрасыр', 'Аль Копчино', 'Четыре нарезона'],
+//     // Пиши код ниже этой строки
+//     checkPizza(pizzaName) {
+//       return this.pizzas.includes(pizzaName);
+//     },
+//     order(pizzaName) {
+//       const isPizzaAvailable = this.checkPizza(pizzaName);
+  
+//       if (!isPizzaAvailable) {
+//         return `В ассортименте нет пиццы с названием «${pizzaName}».`;
+//       }
+  
+//       return `Заказ принят, готовим пиццу «${pizzaName}».`;
+//     },
+//     // Пиши код выше этой строки
+//   };
+  
+
 // ============TASK6============
+// const customer = {
+//     username: 'Mango',
+//     balance: 24000,
+//     discount: 0.1,
+//     orders: ['Burger', 'Pizza', 'Salad'],
+//     // Пиши код ниже этой строки
+//     getBalance() {
+//       return this.balance;
+//     },
+//     getDiscount() {
+//       return this.discount;
+//     },
+//     setDiscount(value) {
+//       this.discount = value;
+//     },
+//     getOrders() {
+//       return this.orders;
+//     },
+//     addOrder(cost, order) {
+//       this.balance -= cost - cost * this.discount;
+//       this.orders.push(order);
+//     },
+//     // Пиши код выше этой строки
+//   };
+  
+//   customer.setDiscount(0.15);
+//   console.log(customer.getDiscount()); // 0.15
+//   customer.addOrder(5000, 'Steak');
+//   console.log(customer.getBalance()); // 19750
+//   console.log(customer.getOrders()); // ['Burger', 'Pizza', 'Salad', 'Steak']
+
 // ============TASK7============
+
+
 // ============TASK8============
 // ============TASK9============
 // ============TASK10===========
